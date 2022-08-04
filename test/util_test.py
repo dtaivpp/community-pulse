1233771617e5daa2a91fd1a8e80004750a0f6334
David Tippett <dtaivpp@gmail.com>
2022-08-04 00:36:30 +0000

    Added simple testing and fixed filtering
    Signed-off-by: David Tippett <dtaivpp@gmail.com>

---
+++ test/util_test.py
@@ -0,0 +1,55 @@
+from os.path import exists
+
+
+def test_matches_filter():
+    from src.util import jsonpath_filters, matches_filter
+    """Tests to ensure json path filtering works"""
+    test_filters = [
+        {
+            "filter": {"username": ["dtaivpp"]},
+            "matches": True
+        },
+        {
+            "filter": {"address.street": ["123 Fake Street"]},
+            "matches": True
+        },
+        {
+            "filter": {"family[*].username": ["remove_me"]},
+            "matches": True
+        },
+        {
+            "filter": {"missing_key": ["fake value"]},
+            "matches": False
+        },
+        {
+            "filter": {"family.username": ["not filtered"]},
+            "matches": False
+        }
+    ]
+    
+    test_doc = {
+        "username": "dtaivpp", 
+        "address": {"street" : "123 Fake Street"},
+        "family": [{"username": "test"}, {"username": "remove_me"}]
+    }
+
+    for filt in test_filters:
+        assert matches_filter(jsonpath_filters(filt["filter"]), test_doc) == filt["matches"]
+
+
+def test_parse_config():
+    pass
+
+
+def test_backoff():
+    from src.util import backoff
+    current = 3
+    expected = 9
+    assert backoff(current) == expected
+
+
+def test_backoff_max():
+    from src.util import backoff
+    current = 20
+    expected = 25
+    assert backoff(current, max_backoff=25) == 25
